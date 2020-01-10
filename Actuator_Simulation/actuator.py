87001aff76b1ccee2687fcda80be629c1f527d2a
gopalk15 <kaylingopal@gmail.com>
2020-01-09 23:10:53 -0800

    Actuataor Driven Case

--- Actuator_Simulation/actuator.py
+++ Actuator_Simulation/actuator.py
@@ -1,42 +1,59 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
+import matplotlib.pyplot as plt 
 
 class EMA:
 
-    def __init__(self,mass,spring_constant):
+    def __init__(self,mass,spring_constant,dampening,dt):
         self.m = mass # Kilograms
         self.k = spring_constant # Newtons per metre
+        self.b = dampening 
+        self.dt = dt # Seconds
+    
 
-    def MassSpring(self,state,t):
-      # unpack the state vector
-      x = state[0]
-      xd = state[1]
+    def acceleration(self,t):
+        pass
+    def velocity(self,state,t):
+        pass
 
-      # these are our constants
-      g = 9.8 # metres per second
+    def position(self,state,t,w):
+    
 
-      # compute acceleration xdd
-      xdd = ((self.k*x)/self.m) + g
+        A1 = 1/((self.m/self.dt**2) + (self.b/self.dt) + (self.k))
+        A2 = (-2*self.m/self.dt**2) - (self.b/self.dt)
+        A3 = (self.m/self.dt**2) 
+        
+        x = np.zeros(len(t))
 
-      # return the two state derivatives
-      return [xd, xdd]
 
+        #unpack state vector
+        x[0],x[1]= state 
+        
+        for i in range(2,len(x)):
+            x[i] = (A1)*(-A2*x[i-1] - A3*x[i-2]) + np.sin(w*t[i])
 
-m = 1.5 #Kilogram
-k = -2.5 #Newtons per metre
+        return x
+        
 
-EMA = EMA(m,k)
+   
+m = 100 #Kilogram
+k = 20 #Newtons per metre
+b = 30 
+dt = 0.1
+freq = 5 #Hz
 
-state0 = [0.0, 0.0]
-t = np.arange(0.0, 10.0, 0.1)
+w = 2*np.pi*freq
 
-state = odeint(EMA.MassSpring, state0, t)
+EMA = EMA(m,k,b,dt)
 
-plt.plot(t, state)
+state = [10, 9]
+
+t = np.arange(0.0, 60.0, dt)
+
+pos = EMA.position(state,t,w)
+
+plt.plot(t, pos)
 plt.xlabel('TIME (sec)')
 plt.ylabel('STATES')
 plt.title('Mass-Spring System')
 plt.legend(('$x$ (m)', '$\dot{x}$ (m/sec)'))
-
 plt.show()
